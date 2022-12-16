908b581d813469870bb7f7a732f79285481f4bea
mozuku55 <ryo.tako.05@gmail.com>
2022-12-17 04:00:28 +0900

    fmt

--- twitterbot/entity/entity.go
+++ twitterbot/entity/entity.go
@@ -1,8 +1,15 @@
 package entity
 
-//keys
+// keys
 type apiKey string
 type accessToken string
 type bearerToken string
 type apiKeySecret string
 type accessTokenSecret string
+type keys = struct {
+	apikey            apiKey
+	accessToken       accessToken
+	bearerToken       bearerToken
+	apiSecretkey      apiKeySecret
+	accessTokenSecret accessTokenSecret
+}
--- twitterbot/keys/keys.go
+++ twitterbot/keys/keys.go
@@ -1,19 +1,23 @@
-import (
-	"entity"
-)
 package keys
-
 import (
 	"github.com/ChimeraCoder/anaconda"
 	."./entity"
 )
 
-type keys = {
-	apiKey apiKey = "PXVdSbxjNOzvmD8UHW1iA61L6",
-	accessToken accessToken = "1603781766887510016-ZrPU9jqE6eZN7YyKr13sKAp3IG7upc",
-	bearerToken bearerToken = "AAAAAAAAAAAAAAAAAAAAAAv5kQEAAAAATR7YmaGTI2rFaWjuZsV7yWL9fGg%3DKbEMw6xWm1gJNeWGl7mGLJga5xNEVuBi23RkrJIzWlACf5wkUn",
-	apiSecretkey : apiKeySecret = "bHTUnMaf4v4Kxh7eVsqzRLHR1lELEEwUfKAuaQvJ8Vev1jwO3P",
-	accessTokenSecret : accessTokenSecret = "mKkpMn0je8r13RSadbyDgxnpjDqNLDa3eSzUn6PtcYKdf",
+type keys = struct{
+	apikey apikey
+	accessToken accessToken
+	bearerToken bearerToken
+	apiSecretkey apiSecretkey
+	accessTokenSecret accessTokenSecret
+}
+
+twitterkey := &keys{
+	apiKey: "PXVdSbxjNOzvmD8UHW1iA61L6",
+	accessToken: "1603781766887510016-ZrPU9jqE6eZN7YyKr13sKAp3IG7upc",
+	bearerToken: "AAAAAAAAAAAAAAAAAAAAAAv5kQEAAAAATR7YmaGTI2rFaWjuZsV7yWL9fGg%3DKbEMw6xWm1gJNeWGl7mGLJga5xNEVuBi23RkrJIzWlACf5wkUn",
+	apiSecretkey: "bHTUnMaf4v4Kxh7eVsqzRLHR1lELEEwUfKAuaQvJ8Vev1jwO3P",
+	accessTokenSecret: "mKkpMn0je8r13RSadbyDgxnpjDqNLDa3eSzUn6PtcYKdf"
 }
 
 func GetTwitterApi() *anaconda.TwitterApi {
